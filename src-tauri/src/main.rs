b05a77970e3e007cbaab04ec74c53be84cd99d88
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-06-10 00:05:36 +0500

    Few optimizations presumably

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -95,9 +95,18 @@ fn command_line_convert(modpreset: &str, backticks: bool) -> Result<ModData, Str
 
 #[tauri::command]
 async fn orbat_generate(orbat: HashMap<String, u64>) -> Result<String, String> {
-    dbg!(&orbat);
+    // dbg!(&orbat);
+    let mut roles = vec![];
 
-    Ok(format!("{:#?}", orbat))
+    for (role, amount) in orbat.iter() {
+        if !role.is_empty() && *amount > 0 {
+            roles.push((amount.to_string(), role.to_owned()));
+        }
+    }
+
+    let roles_emojis_vec = convert_roles(roles);
+
+    Ok(roles_emojis_vec.0.join("\n"))
 }
 
 #[tauri::command]
@@ -108,18 +117,32 @@ async fn orbat_convert(orbat: String) -> Result<String, String> {
         let line = line.split_once(" ");
         if let Some(line) = line {
             let (amount, role) = line;
-            dbg!(amount, role);
-            let role_enum = Role::from_str(role).expect("Role unable to be converted");
-            roles.push((amount, role_enum));
+            // let role_enum = Role::from_str(role).expect("Role unable to be converted");
+            roles.push((amount.to_string(), role.to_string()));
         }
     }
+    let (roles_vec, emojis_vec) = convert_roles(roles);
 
+    Ok(dbg!(format!(
+        "{}\n\n{}",
+        roles_vec.join("\n"),
+        emojis_vec.join(" ")
+    )))
+}
+
+fn convert_roles(roles: Vec<(String, String)>) -> (Vec<String>, Vec<String>) {
+    let mut roles = roles.into_iter().filter_map(
+        |role_tuple|        match Role::from_str(&role_tuple.1) {
+            Ok(role) => {
+                Some((role_tuple.0, role))
+            }
+            Err(error) => {
+                dbg!(role_tuple.0, role_tuple.1, error);
+                None
+            }
+        }
+    ).collect::<Vec<(String, Role)>>();
     roles.sort_by(|first, second| first.1.cmp(&second.1));
-
-    let mut roles = roles
-        .into_iter()
-        .map(|item| (item.0.to_string(), item.1))
-        .collect::<Vec<(String, Role)>>();
 
     let zeus_present = dbg!(roles.iter().any(|role| format!("{:?}", role.1)
         .to_string()
@@ -129,21 +152,15 @@ async fn orbat_convert(orbat: String) -> Result<String, String> {
         roles.insert(0, ("2x".to_string(), Role::Zeus));
     }
 
-    let emojis = roles
-        .iter()
-        .map(|role| format!(":{:?}:", role.1))
-        .collect::<Vec<String>>();
+    let mut roles_vec = vec![];
+    let mut emojis_vec = vec![];
 
-    Ok(dbg!(format!(
-        "{}\n\n{}",
-        roles
-            .iter()
-            .map(|role| { format!("{} {:?}", role.0, role.1) })
-            .collect::<Vec<String>>()
-            .join("\n")
-            .trim(),
-        emojis.join(" ")
-    )))
+    for (role_count, role) in roles {
+                roles_vec.push(format!("{} {:?}", role_count, role));
+                emojis_vec.push(format!(":{:?}:", role));
+            }
+
+    (roles_vec, emojis_vec)
 }
 
 fn main() {
